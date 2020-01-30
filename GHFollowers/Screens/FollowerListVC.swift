12f04e943bded8104407601cf8cabc26fc4d6936
Miguel <gannasong@gmail.com>
2020-01-31 00:55:47 +0800

    setup flowLayout

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -11,12 +11,51 @@ import UIKit
 class FollowerListVC: UIViewController {
 
   var username: String!
+  var collectionView: UICollectionView!
+
+  // MARK: - UIViewController
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    configureViewController()
+    configureCollectionView()
+    getFollowers()
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    navigationController?.setNavigationBarHidden(false, animated: true)
+  }
+
+  // MARK: - Private Methods
+
+  private func configureViewController() {
     view.backgroundColor = .systemBackground
     navigationController?.navigationBar.prefersLargeTitles = true
+  }
 
+  private func configureCollectionView() {
+    collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+    view.addSubview(collectionView)
+    collectionView.backgroundColor = .systemPink
+    collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+  }
+
+  private func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+    let width = view.bounds.width
+    let padding: CGFloat = 12
+    let minimumItemSpacing: CGFloat = 10
+    let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+    let itemWidth = availableWidth / 3
+
+    let flowLayout = UICollectionViewFlowLayout()
+    flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+    flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+
+    return flowLayout
+  }
+
+  private func getFollowers() {
     NetworkManager.shared.getFollowers(for: username, page: 1) { (result) in
       switch result {
         case .success(let followers):
@@ -26,10 +65,5 @@ class FollowerListVC: UIViewController {
           self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
       }
     }
-  }
-
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-    navigationController?.setNavigationBarHidden(false, animated: true)
   }
 }
